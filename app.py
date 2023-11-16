5e695b62a5efbdd41563c2038887b82c71576552
Alessandro Ercolani <ale.ercolani@gmail.com>
2023-11-16 21:43:22 +0100

    working cloud file storage

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ import os
 from dotenv import load_dotenv
 import stripe
 import flask
+import boto3
+import io
+
+
+s3 = boto3.client('s3',
+    aws_access_key_id=os.getenv('AWS_SERVER_PUBLIC_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SERVER_SECRET_KEY') )
 
 TOKEN_DB = {"asdf1234567890": {"uid": 100}}
 
@@ -36,28 +43,45 @@ def get_secret(user) -> str:
 
 def create_product(user, body) -> str:
     b64 = body['imagebase64']
+    color = body['color']
+    name = body['name']
+
     im = Image.open(BytesIO(base64.b64decode(b64)))
     imgbk = Image.open(r"./data/hoodie-black-retro.png") 
 
     img3 = im.resize((300,300))
   
     imgbk.paste(img3, (250,280)) 
-    imgbk.save(r"./static/test.png")
+
     ENV_URL = os.getenv('ENV_URL')
-    image_url = f'{ENV_URL}/static/test.png'
-    image_front = f'{ENV_URL}/static/hoodie-black-front.png'
+    image_url = f'https://hoodie-creator.s3.eu-west-1.amazonaws.com/{name}'
+    #image_front = f'{ENV_URL}/static/hoodie-black-front.png'
     print(image_url)
-    description_stripe = '80x80 cm black wood framed print send directly to your home'
+    description_stripe = 'hoodie'
     
     metadata_stripe = {"size" : "M", 
                         "color" :  'black', 
-                        "path_image" : './data/test.png',
+                        "url_image" : image_url,
                         }
     
+    in_mem_file = io.BytesIO()
+    imgbk.save(in_mem_file, format=imgbk.format)
+    in_mem_file.seek(0)
+
+    s3.upload_fileobj(
+        in_mem_file, # This is what i am trying to upload
+        'hoodie-creator',
+        name,
+        ExtraArgs={
+            'ACL': 'public-read',
+            'ContentType': 'image/png'
+        }
+    )
+    
     try:
-        product = stripe.Product.create(name='test', 
+        product = stripe.Product.create(name=name, 
                                 description=description_stripe, 
-                                images=[image_url,image_front],   
+                                images=[image_url, 'https://hoodie-creator.s3.eu-west-1.amazonaws.com/test.png'],   
                                 default_price_data={"unit_amount": 8990, "currency": "eur"},
                                 metadata=metadata_stripe)
         print(product)
@@ -89,69 +113,11 @@ def create_product(user, body) -> str:
         return str(e)
     return  {'link_stripe' : checkout_session.url}
     
-'''
-    try:
-        product = stripe.Product.create(name=name, 
-                                description=description_stripe, 
-                                images=[url_image],   
-                                default_price_data={"unit_amount": 19900, "currency": "eur"},
-                                metadata=metadata_stripe)
-        print(product)
-        product_id = product['id']
-        price= stripe.Price.create(
-            unit_amount=19900,
-            currency="eur",
-            product=product_id,
-        )
-        print(price)
-        price_id = price['id']
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': price_id,
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success.html',
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
-                shipping_address_collection={
-                    'allowed_countries': ['IT'],
-                }
-        )
-      
-        metadata = NFTMetadataInput.from_json({
-                    "name": name_nft,
-                    "description": description_nft,
-                    "image": image_nft
-                })
-                
-                # 0xB3053C6baAc7eAC6e0F9037f1E2C92a0f5f6e094
-        tx = current_app.nft_module.mint_to("0xB3053C6baAc7eAC6e0F9037f1E2C92a0f5f6e094", metadata)
-        receipt = tx.receipt
-        token_id = tx.id
-        nft = tx.data()
-        print(nft)
-
-        metadata_stripe["id_nft"] = str(token_id)
-        stripe.Product.modify(
-                product_id,
-                metadata=metadata_stripe,
-            )
-  except Exception as e:
-        print(str(e))
-        return str(e)
-    return  {'link_stripe' : checkout_session.url}
-'''
-
-
 app = connexion.FlaskApp(__name__, specification_dir="spec", )
 app.add_api("openapi.json")
 load_dotenv()
 
 stripe.api_key  = os.getenv('STRIPE_SECRET_KEY')
-
 
 if __name__ == "__main__":
     app.run(f"{Path(__file__).stem}:app", port=80)
