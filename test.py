3e56a6504336ab3645e17da122177909bead1274
Alessandro Ercolani <ale.ercolani@gmail.com>
2023-11-17 15:29:37 +0100

    fix

--- test.py
+++ test.py
@@ -11,6 +11,23 @@ client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
 print(client.files.list())
 #client.files.retrieve("file-iWtdUVELk3tPISWLcBkRuh6Z")
 
+from PIL import Image
+import base64
+from io import BytesIO
+
+# Load the image
+image_path = './data/lynx.png'
+image = Image.open(image_path)
+
+
+buffered = BytesIO()
+image.save(buffered, format="PNG")
+img_str = base64.b64encode(buffered.getvalue()).decode()
+
+print(img_str)  # Displaying the first 100 characters of the base64 string as a sanity check
+
+
+
 
 '''  
 img1 = Image.open(r"./data/hoodie-black-retro.png") 
